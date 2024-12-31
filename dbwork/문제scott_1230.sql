a74e70fa708b7835b90c3f3934271f701488a020
Eunyoung-Choe <ceytown@student.hywoman.ac.kr>
2024-12-31 10:20:18 +0900

    sql문제 주석 추가

--- dbwork/문제scott_1230.sql
+++ dbwork/문제scott_1230.sql
@@ -4,7 +4,7 @@
 
 -- 2. ename에 대소문자 상관없이 's'를 포함하고 있는 데이터 출력
 --    (컬럼: ename, job, sal)
-    select ename, job, sal from emp where lower(ename) like '%s%';
+    select ename, job, sal from emp where lower(ename) like '%s%'; --모든 문자를 lower/upper로 만들고 찾는 방식!
 
 -- 3. ename의 3번째 글자가 L인 사람을 조회하기 (ename, sal, comm)
     select ename from emp where ename like '__L%';
@@ -15,11 +15,13 @@
     
 
 -- 5. hiredate 입사일이 5월인 사람은 모두 출력 (ename, hiredate, sal)
-    SELECT ENAME, to_char(hiredate, 'yyyy-mm-dd') 입사일, SAL FROM EMP WHERE to_char(hiredate, 'mm') = 05; --어렵
+    SELECT ENAME, to_char(hiredate, 'yyyy-mm-dd') 입사일, SAL FROM EMP WHERE to_char(hiredate, 'mm') = '05'; --어렵
+    --참고로 오라클은 숫자5로 써도 비교가 가능하다.
 
 
 -- 6. hiredate 입사일이 1985-01-01 이후에 입사한사람 출력(컬럼 : ename, hiredate, sal)
     select ename, to_char(hiredate, 'yyyy-mm-dd'), sal from emp where hiredate > TO_DATE('1985-01-01', 'yyyy-mm-dd'); --to_date 기억
+    select ename, hiredate, sal from emp where hiredate >= '1985-01-01'; --쌤 답안 
 
 -- 7. sal이 1500~3000 사이인 사람을 출력하는데 관계연산자와 and를 이용해서 출력
     select ename, sal from emp where sal>1500 and sal<=3000;
@@ -31,7 +33,7 @@
     select * from emp where job in ('CLERK', 'PRESIDENT', 'MANAGER'); --컬럼명 대문자인 점 주의 
 
 -- 10. ename, sal, comm, sal*comm을 출력하는데 comm이 null인 경우 1로 변경해서 출력하고 계산하기
-    select ename, sal, NVL(comm,1), sal*NVL(comm,1) from emp;
+    select ename, sal, NVL(comm,1), sal*NVL(comm,1) from emp; --NVL=Null Value Logic : NULl값 대체를 위한 기능 
 
 -- 11. ename이 ADAMS인 사람의 SAL보다 더 많이 받는 사람 출력(컬럼명 : ENAME, JOB, SAL)
     select ename, job, sal from emp where sal > (select sal from emp where ename='ADAMS'); --자꾸 ==으로 쓰게 된다;
@@ -43,7 +45,8 @@
     select ename from emp where ename like 'A%' or ename like 'S%' or ename like 'M%';
 
 -- 14. MGR을 GROUP로 인원수와 평균 SAL을 구하시오(평균 SAL은 무조건 올림으로)
-    select count(*), ceil(avg(sal)), mgr from emp group by mgr; --평균 소수점 쓰려면 round()로.
+    select count(*) 인원수, round(avg(sal), -1) 평균급여, mgr from emp where mgr is not null group by mgr; --평균 소수점 쓰려면 round()로.
+    -- 1원 단위는 반올림
     
 -- 15. SCOTT의 SAL과 같은 SAL을 받는사람을 조회하시오(컬럼 : ENAME, SAL)
     select ename, sal from emp where sal = (select sal from emp where ename='SCOTT');
@@ -55,8 +58,8 @@
     select ename 이름, job 직업 from emp where ename like '__R%' OR ename like '__A%';
     
 -- 18. JOB 직업별로 인원수와 최고 연봉을 출력하는데 직업의 오름차순으로 출력
-    select job 직업, count(*), max(sal) from emp group by job order by job;
+    select job 직업, count(*), max(sal) from emp group by job order by 1;
 
 -- 19. || 연산자를 이용하여 다음과 같이 하나의 컬럼으로 출력하시오
 ----      (예) SCOTT의 직업은 CLERK이며 입사년도는 1989년 05월입니다.
-    select ename||'의 직업은 '||job||'이며 입사년도는 '||to_char(hiredate, 'yyyy-mm-dd')||'입니다.' 자기소개 from emp;
+    select ename||'의 직업은 '||job||'이며 입사년도는 '||to_char(hiredate, 'yyyy')||'년 '||to_char(hiredate, 'mm')||'월 '||to_char(hiredate, 'dd')||'일입니다.' 자기소개 from emp;
